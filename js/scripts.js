080c5c608053e4e325a38aa843e6933a73089cf9
Student <example@example.com>
2018-06-21 11:56:48 -0700

    Move the saved beats below with position absolute

--- js/scripts.js
+++ js/scripts.js
@@ -190,19 +190,23 @@ $(document).ready(function() {
       beatWan.saveBeat();
       beatWan.savedArray();
       $(".savedBeats").show();
-      $("#listOfBeats").append("<li value='" + saved + "'>"+ "beat " + (saved + 1) + "</li>");
+      $("#listOfBeats").append("<li value='" + saved + "'>"+ "Beat " + (saved + 1) +" <img src=\"img/trash.png\" class=\"trash icon\"></li>");
       saved++;
     }
   });
 
   $("#listOfBeats").on('click', 'li', function(){
     var chosenBeat = beatWan.savedBeats[$(this).val()];
+    $(this).children().toggle();
     $(".beatsAll .spot").removeClass("selected snare bass hihat bongo keys");
     $(".beatsAll .spot").each(function(i){
         if(chosenBeat[i] === "selected") {
         $(this).addClass("selected snare bass hihat bongo keys");
       }
     });
+    $(".trash").click(function() {
+      $(this).parent().remove();
+    })
   });
 
 
